292dba9815a20c514cde3b605cc97bf95af78ee2
overusedname <104315428+skiddyshit@users.noreply.github.com>
2025-12-29 00:09:03 +0300

    Create autoleave.lua

---
+++ autoleave.lua
@@ -0,0 +1,17 @@
+local Players = game:GetService("Players")
+
+local queueteleport = queue_on_teleport or (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
+
+task.spawn(function()
+    task.wait(20)
+    game:Shutdown()
+end
+
+local TeleportCheck = false
+
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+	if (not TeleportCheck) and queueteleport then
+		TeleportCheck = true
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/skiddyshit/autoleave/refs/heads/main/autoleave.lua'))()")
+  end
+end)
